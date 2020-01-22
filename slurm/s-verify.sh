b987a8ece0b9c779ece15b8693099b936fc48453
David E. Narváez <david.narvaez@computer.org>
2020-01-22 08:47:38 -0500

    Run the Whole Toolchain from One Script
    While testing this in TACC, some jobs were producing empty/incomplete
    outputs which seems to be related to data transfer issues. Running the
    whole toolchain from one script guarantees that all files are local.

--- slurm/s-verify.sh
+++ slurm/s-verify.sh
@@ -7,7 +7,5 @@ PARTITION="normal"
 for i in $(ls -1 s$1*.ippr); do
     basename=$(basename $i .ippr);
     echo $basename;
-    pprsearchjid=$(sbatch --parsable -t $TIMELIMIT -N 1 -n 1 -p $PARTITION -o $basename.ppr -e $basename.pprsearch.err "$SLURMDIR/pprsearch.sh" $basename.cnf $i | tail -1);
-    ppr2dratjid=$(sbatch -d "after:$pprsearchjid" --parsable -t $TIMELIMIT -N 1 -n 1 -p $PARTITION -o $basename.drat -e $basename.ppr2drat.err "$SLURMDIR/ppr2drat.sh" $basename.cnf $basename.ppr | tail -1);
-    sbatch -d "after:$ppr2dratjid" -t $TIMELIMIT -N 1 -n 1 -p $PARTITION -o $basename.log -e $basename.drat-trim.err "$SLURMDIR/drat-trim.sh" $basename.cnf $basename.drat -f;
+    sbatch -t $TIMELIMIT -N 1 -n 1 -p $PARTITION -o $basename.log "$SLURMDIR/toolchain.sh" $basename;
 done
